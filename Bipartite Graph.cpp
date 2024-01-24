a6c3647975c4a1c869245eb4e2c86680434485be
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-01-24 14:13:55 +0530

    BFS & DFS

--- Bipartite Graph.cpp
+++ Bipartite Graph.cpp
@@ -21,6 +21,7 @@
 
 using namespace std;
 
+//BFS
 bool check(int start, int V, vector<int>adj[], vector<int> &color)
 {
     queue<int> q;
@@ -55,6 +56,42 @@ bool isBipartite(int V, vector<int>adj[])
         if(color[i] == -1)
         {
             if(check(i, V, adj, color) == false)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool dfs(int node, int col, vector<int> &color, vector<int> adj[])
+{
+    color[node] = col;
+    for(auto it : adj[node])
+    {
+        if(color[it] == -1)
+        {
+            if(dfs(it, !col, color, adj) == false)
+            {
+                return false;
+            }
+        }
+        else if(color[it] == col)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isBipartite(int V, vector<int>adj[])
+{
+    vector<int> color(V, -1);
+    for(int i = 0; i <V; i++)
+    {
+        if(color[i] == -1)
+        {
+            if(dfs(i, 0, color, adj) == false)
             {
                 return false;
             }
