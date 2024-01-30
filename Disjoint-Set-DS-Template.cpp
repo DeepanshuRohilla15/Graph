81af5d3316caa469cf4a9544406bffa15f33b32b
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-01-31 00:23:06 +0530

    Disjoint Set DS Tempate

---
+++ Disjoint-Set-DS-Template.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <algorithm>
+#include <sstream>
+#include <stack>
+#include <iomanip>
+#include <climits>
+#include <assert.h>
+#include <chrono>
+
+using namespace std;
+
+class DisjointSet{
+    vector<int> parent, rank, size;
+public:
+    DisjointSet(int n)
+    {
+        rank.resize(n+1, 0);
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i = 0; i <=n; i++)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node)
+    {
+        if(node == parent[node])
+        {
+            return node;
+        }
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u == ulp_v) return;
+
+        if(rank[ulp_u] < rank[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+        }
+        else if(rank[ulp_v] < rank[ulp_u])
+        {
+            parent[ulp_v] = ulp_u;
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionByRank(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u == ulp_v) return;
+
+        if(size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else{
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+int main()
+{
+    DisjointSet ds(7);
+    ds.unionByRank(1, 2);
+    ds.unionByRank(2, 3);
+    ds.unionByRank(4, 5);
+    ds.unionByRank(6, 7);
+    ds.unionByRank(5, 6);
+    // if 3 and 7 same or not
+    if(ds.findUPar(3) == ds.findUPar(7))
+    {
+        cout<<"Same\n";
+    }
+    else{
+        cout<<"Not Same\n";
+    }
+
+    ds.unionByRank(3, 7);
+    if(ds.findUPar(3) == ds.findUPar(7))
+    {
+        cout<<"Same\n";
+    }
+    else{
+        cout<<"Not Same\n";
+    }
+}
